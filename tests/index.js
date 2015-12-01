683cd83956c62a54b85b39e8b251df59689446b9
Greg Tatum <tatum.creative@gmail.com>
2015-11-30 20:32:44 -0600

    Change up test runner

--- tests/index.js
+++ tests/index.js
@@ -1,2 +1,12 @@
-require('./material/flat/flat-test')
-require('./renderer/straight-test')
+var Tape = require('tape')
+
+try {
+	require('./material/flat/flat-test')
+	require('./renderer/straight-test')
+} catch( err ) {
+	// catch errors in electron, fail the tests to get a proper exit code
+	Tape("javascript error", function(t) {
+		t.plan(1)
+		t.fail(err)
+	})
+}
