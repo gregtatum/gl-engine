6f341f05a11d634fef4ae2112939bc1a0f5eec53
Greg Tatum <tatum.creative@gmail.com>
2015-12-16 19:48:55 -0700

    Lit wireframe example

---
+++ docs/recipes/coming-soon.md
@@ -0,0 +1,3 @@
+# Recipes
+
+Looking to cook something up interesting? Follow along with these simple recipes to get your development kick started.
---
+++ examples/05-lit-wireframe/lit-wireframe-old.js
@@ -0,0 +1,122 @@
+/**
+ * Glam - A WebGL Rendering Engine
+ * https://github.com/glamjs/glam
+ *
+ * This demo shows off how to create a lit surface using directional lights and
+ * a Lambert reflectance model.
+ */
+
+// Load in all of the dependencies to start
+var Bunny = require('bunny')
+var Normals = require('normals')
+var Glam = require('glam')
+var Vec3 = require('gl-vec3')
+var CreateVignette = require('gl-vignette-background')
+var WireframeCells = require('gl-wireframe')
+
+// Simulate ES6 `import { ThingA, ThingB } from 'glam'`
+var PerspectiveCamera  = Glam.PerspectiveCamera
+var Geometry           = Glam.Geometry
+var LitMaterial        = Glam.LitMaterial
+var Mesh               = Glam.Mesh
+var ForwardRenderer    = Glam.ForwardRenderer
+var Scene              = Glam.Scene
+var Transform          = Glam.Transform
+var LambertAugment     = Glam.LambertAugment
+var DirectionalLight   = Glam.DirectionalLight
+
+// This is the main function that will run our code
+;(function runImmediately() {
+
+	var scene          = Scene()
+	var camera         = PerspectiveCamera()
+	var mesh           = createBunnyMesh( scene )
+	var lights         = createLights( scene )
+	
+	createAndRenderBackground( scene )
+	
+	scene.on('update', function( event ) {
+		
+		// Rotate the bunny mesh with the elapsed time using euler angles
+		mesh.euler[1] = event.elapsed * 0.001
+		mesh.euler[0] = Math.sin( event.elapsed * 0.0001 )
+		
+		// Now render the scene
+		scene.render( camera )
+	})
+})()
+
+function createLights( scene ) {
+	
+	var lights = []
+	
+	lights[0] = DirectionalLight({ color: [ 0.8, 0.5, 0.3 ], direction: [ 0.5, -0.5, 0.5 ] })
+	lights[1] = DirectionalLight({ color: [ 0.9, 0.9, 1.0 ], direction: [ 0.0, 1.0, 0.0 ] })
+	lights[2] = DirectionalLight({ color: [ 0.1, 0.3, 0.4 ], direction: [ -0.5, -0.3, 0.2 ] })
+	
+	lights.forEach(function( light ) {
+		Vec3.scale( light.color, light.color, 0.5 )
+		scene.add( light )
+	})
+	
+	return lights
+}
+
+function createBunnyMesh( scene ) {
+	
+	// Create the bunny mesh, which is a collection of a geometry and material.
+	
+	// Set up a lit material with the lambert reflectance model
+	var material =
+		LitMaterial({
+			color: [0.5,0.5,0.5] // Ambient color
+		})
+		.use( LambertAugment, {
+			diffuse: [1,1,1]
+		})
+
+	// Our bunny model didn't come with normals, so add them here
+	Bunny.normals = Normals.vertexNormals(Bunny.cells, Bunny.positions)
+	
+	// Feed the bunny "simplicial complex" into a Glam geometry
+	var geometry = Geometry( Bunny )
+		
+	geometry.data.cells = WireframeCells( Bunny.cells )
+	material.mode = "LINES"
+	var mesh     = Mesh( material, geometry )
+	
+	mesh.position[1] = -5
+	mesh.position[2] = -20
+	
+	scene.add( mesh )
+	
+	return mesh
+}
+
+function createAndRenderBackground( scene ) {
+	
+	// Use gl-vignette-background to create a nice background
+	
+	var gl = scene.renderer.gl
+	var background = CreateVignette( gl )
+	
+	// Style the background
+	background.style({
+        color1: [0.45, 0.48, 0.5],
+        color2: [0.0, 0.05, 0.1],
+        smoothing: [ -0.5, 1.0 ],
+        noiseAlpha: 0.1,
+        offset: [ 0, -0.25 ],
+	})
+	
+	// Use the beforerender event to render the background. This
+	// hijacks the scene graph and allows gl-vignette-background to use its
+	// own gl calls. Temporarily disable depth testing so that it will always
+	// be behind the model.
+	
+	scene.on('beforerender', function() {
+		gl.disable( gl.DEPTH_TEST )
+		background.draw()
+		gl.enable( gl.DEPTH_TEST )
+	})
+}
