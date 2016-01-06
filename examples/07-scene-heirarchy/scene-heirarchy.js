331371f430f9a47d8c0df9e45219cbb2d0f3a6d2
Greg Tatum <tatum.creative@gmail.com>
2016-01-05 18:42:46 -0600

    Comments on example 07

--- examples/07-scene-heirarchy/scene-heirarchy.js
+++ examples/07-scene-heirarchy/scene-heirarchy.js
@@ -1,98 +1,120 @@
 //	Glam - A WebGL Rendering Engine: https://github.com/glamjs/glam
-//	This demo shows off how to create a lit surface using directional lights and
-//	a Lambert reflectance model.
+//	This demonstrates how to work with a scene and parenting items
 
-var Box = require('geo-3d-box')
-var Glam = require('glam')
-var Vec3 = require('gl-vec3')
 var CreateVignette = require('gl-vignette-background')
+var Box   = require('geo-3d-box')
+var Glam  = require('glam')
+var Vec3  = require('gl-vec3')
 var Color = require('@tatumcreative/color')
 
 Glam.Engine( function onReady( engine, scene ) {
 
-	var camera = Glam.PerspectiveCamera()
-	var lights = createLights( scene )
+	// Create the mesh with 5 lines of boxes
+	var mesh       = createMesh( scene )
+	var rootMeshes = scene.children( mesh )
+	var segments   = scene.getByType('segment')
 
-	scene.add( camera )
+	// Setup the rest of the scene
+	var camera = createCamera( scene ) 
+	var lights = createLights( scene )
 	createAndRenderBackground( engine.renderer )
 
-	var thing = createThing( scene )
-	var roots = scene.children( thing )
-	var fingerlings = scene.getByType('fingerling')
-
+	// The update/draw loop
 	engine.on('update', function( event ) {
-		camera.position[2] = 50
-		camera.position[1] = 10
-		// Now render the scene
 
-		for( var i=0; i < fingerlings.length; i++ ) {	
-			var mesh = fingerlings[i]
+		// Spin the mesh slowly around
+		mesh.euler[2] = event.elapsed * 0.0005
+		
+		// Wave the lines segments around
+		segments.forEach(function(segment) {
+			segment.euler[2] = (Math.sin( event.elapsed * 0.001 ) + 1) / 4
+			segment.euler[1] = Math.sin( event.elapsed * 0.0007 )
+		})
 
-			mesh.euler[2] = (Math.sin( event.elapsed * 0.001 ) + 1) / 4
-			mesh.euler[1] = Math.sin( event.elapsed * 0.0007 )
-		}
-
-		thing.euler[2] = event.elapsed * 0.0005
-	
-		for( var i=0; i < roots.length; i++ ) {	
-			var root = roots[i]
-
+		// Make the roots of the lines wave around creepily
+		rootMeshes.forEach(function(root, i) {
 			root.euler[0] = 0
 			root.euler[1] = Math.sin( (2 * i / 5) * Math.PI )
 			root.euler[2] = Math.PI * i / (5 / 2)
-		}
+		})
 
+		// Render the scene
 		scene.render( camera )
 	})
 })
 
-function createThing( scene ) {
+function createMesh( scene ) {
+
+	// Create a simplicial complex
 	var simplicial = Box({ size: [1.2, 3, 1.2] })
 
+	// Move the box up so the base is at the origin
 	simplicial.positions.forEach(function( position ) {
 		position[1] += 1.5
 	})
 
+	// Turn the simplicial complex into a glam geometry
 	var geometry = Glam.Geometry( simplicial )
-	var material = Glam.LitMaterial
-	material = Glam.LambertAugment( material )
-	material = Glam.FogAugment( material, {
+
+	// Combine some augments to make a custom material factory function
+	var makeMaterial = Glam.LambertAugment( Glam.LitMaterial )
+	makeMaterial = Glam.FogAugment( makeMaterial, {
 		color : [0.5,0.5,0.5],
 		near : 30,
 		far: 100
 	})
-	
-	var thing = Glam.Mesh()
-	
-	thing.euler[0] = -Math.PI / 2
 
+	// Make the base mesh
+	var mesh = Glam.Mesh()
+	mesh.euler[0] = -Math.PI / 2
+
+	// Make 5 lines of segments
 	for( var j=0; j < 5; j++ ) {
-		var node = thing
+		var node = mesh
+
+		// Each line has 100 segments, pass in the previous segment
 		for( var i=0; i < 100; i++ ) {
-			node = createMesh( scene, node, geometry, material, i )
+			node = createSegment( scene, node, geometry, makeMaterial, i )
 		}
 	}
 	
+	// Add the mesh to the scene
+	scene.add( mesh )
 	
-	scene.add( thing )
-	
-	return thing
+	return mesh
 }
 
-function createMesh( scene, parent, geometry, makeMaterial, i ) {
+function createSegment( scene, parent, geometry, makeMaterial, i ) {
 	
+	// Make each material a slightly different color
 	var material = makeMaterial()
 	material.shading.lambert.diffuse = Color.hslToRgb([ (0.5 + i / 100) % 1, 1, 0.5 ])
 
+	// Create the mesh
 	var mesh = Glam.Mesh( geometry, material )
-	mesh.type.push('fingerling')
+	
+	// Add a custom type to the mesh, so it can be selected easier later on
+	mesh.type.push('segment')
+
+	// Offset the mesh, and scale it down
 	mesh.position[1] = i > 0 ? 3 : 0
 	mesh.scale[0] = 0.96
 	mesh.scale[1] = 0.96
 	mesh.scale[2] = 0.96
+
+	// Finally parent the mesh in the scene graph to the previous mesh
 	scene.add( parent, mesh )
 	
 	return mesh
+}
+
+function createCamera( scene ) {
+	var camera = Glam.PerspectiveCamera()
+	camera.position[2] = 50
+	camera.position[1] = 10
+	scene.add( camera )
+
+	return camera
 }
 
 function createLights( scene ) {
@@ -114,7 +136,7 @@ function createLights( scene ) {
 	
 	// Scale down the color and add the lights
 	lights.forEach(function( light ) {
-		Vec3.scale( light.color, light.color, 0.5 )
+		Vec3.scale( light.color, light.color, 0.7 )
 		scene.add( light )
 	})
 	
